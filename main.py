635cc2d132ccf24c2292c4964520b9d75b36314d
Артём Лебедев <wartemka12@yandex.ru>
2022-12-03 22:20:15 +0530

    Annotation complete

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+def main() -> None:
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
